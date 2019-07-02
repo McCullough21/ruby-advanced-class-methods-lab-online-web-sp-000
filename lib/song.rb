fc30378c852c19193690086d63de2049e31e2740
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-02 15:10:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,6 +29,8 @@ def self.find_or_create_by_name(name)
   self.find_by_name(name) || self.create_by_name(name)
 end
 
+def self.alphabetical
+  @@all.sort_by{|song| song.name}
   def self.all
     @@all
   end
