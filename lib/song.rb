7fdc095d1c4e3183a5487ce1164948bdca2e848b
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-02 14:28:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,20 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+
+def self.create
+  song = self.new
+  song_name = name
+  @@all << name
+  song
+end
+
+def self.new_by_name(name)
+  song = self.new
+  @name = name
+  song = @name
+
+end
   def self.all
     @@all
   end
