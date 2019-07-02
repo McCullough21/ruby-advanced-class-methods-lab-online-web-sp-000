fcd5174b7d9c09188d83d7e7d099e237a162a86b
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-02 14:52:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,12 @@ def self.new_by_name(name)
   song = self.new
   song.name = name
   song
+end
 
+def create_by_name(name)
+  song = self.new
+  song.name = name
+  @@all << name
 end
 
   def self.all
