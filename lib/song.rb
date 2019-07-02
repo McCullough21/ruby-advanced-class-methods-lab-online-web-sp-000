579c5601af635105dcaadaaa69c09f22a8049eed
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-02 15:03:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,7 +26,9 @@ def self.find_by_name(name)
   self.all.find {|song| song.name == name}
 end
 def self.find_or_create_by_name(name)
-  self.create_by_name(name) if !self.find_by_name(name)
+   if !self.find_by_name(name)
+       self.create_by_name(name) 
+   end
   return name
 end
 
